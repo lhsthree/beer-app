441e89c2d0f8c4e1ff1936677dcbab5f23f8b9a9
lhsthree <lhsthree@gmail.com>
2020-09-17 17:02:39 -0500

    feat: adding pagination and ABV and IBU API links

--- index.js
+++ index.js
@@ -1,15 +1,83 @@
-const urlBase = "https://api.punkapi.com/v2/beers";
+const urlBase = "https://api.punkapi.com/v2/beers?page=";
+const filterABV = document.getElementById('filterABV');
+const filterIBU = document.getElementById('filterIBU');
+const pageText = document.getElementById('pageNumber');
+const prevPage = document.getElementById('prevPage');
+const nextPage = document.getElementById('nextPage');
+let optionsIBU = "";
+let optionsABV = "";
+let page = 1;
+
+filterABV.addEventListener("change", e => {
+	const value = e.target.value;
+
+	switch (value){
+		case "all":
+			optionsABV = "";
+			break
+		case "weak":
+			optionsABV = "&abv_lt=4.6";
+			break
+		case "medium":
+			optionsABV = "&abv_gt=4.5&abv_lt=7.6";
+			break
+		case "strong":
+			optionsABV = "&abv_gt=7.5";
+			break
+	}
+
+	page = 1;
+	getBeers();
+});
+
+filterIBU.addEventListener("change", e => {
+	const value = e.target.value;
+
+	switch (value){
+		case "all":
+			optionsIBU = "";
+			break
+		case "weak":
+			optionsIBU = "&ibu_lt=35";
+			break
+		case "medium":
+			optionsIBU = "&ibu_gt=34&ibu_lt=75";
+			break
+		case "strong":
+			optionsIBU = "&ibu_gt=74";
+			break
+	}
+
+	page = 1;
+	getBeers();
+});
 
 async function getBeers(){
-	const response = await fetch(urlBase)
+	const url = urlBase + page + optionsABV + optionsIBU;
+
+	const response = await fetch(url)
 	let beers = await response.json()
 	console.log(beers[0])
+
+	pageText.innerText = page;
+
+	if(page === 1) {
+		prevPage.disabled = true;
+	}else {
+		prevPage.disabled =false;
+	}
+
+	if(beers.length < 25){
+		nextPage.disabled = true;
+	}else {
+		nextPage.disabled = false;
+	};
 
 	const beersDiv = document.querySelector('.beers');
 
 	let beerHtml = "";
 
-	beers.forEach(beer =>{
+	beers.forEach(beer => {
 		beerHtml += `
 
 		<div class="beer-wrapper card">
@@ -36,5 +104,14 @@ async function getBeers(){
 
 	beersDiv.innerHTML = beerHtml;
 }
+
+prevPage.addEventListener('click', () => {
+	page--;
+	getBeers();
+})
+nextPage.addEventListener('click', () => {
+	page++;
+	getBeers();
+})
 
 getBeers()
